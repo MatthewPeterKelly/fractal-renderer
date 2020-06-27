8eb4fbc741a9e2be192ea0041220d7e2702a4c01
Matt Kelly <matthew.kelly2@gmail.com>
2020-06-27 10:21:16 -0400

    simple program to echo user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    let mut user_input = String::new();
+
+    io::stdin().read_line(&mut user_input).expect("Failed to read user input!");
+
+    println!("You entered: {}", user_input.trim());
+
 }
