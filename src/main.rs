37ea6e7e3c47d60e0664bfd3b01ecb3531444088
Matt Kelly <matthew.kelly2@gmail.com>
2020-06-27 10:34:57 -0400

    enter a number with input validation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
-    let mut user_input = String::new();
+    let mut user_input_buffer = String::new();
 
-    io::stdin().read_line(&mut user_input).expect("Failed to read user input!");
+    println!("Enter a number...");
 
-    println!("You entered: {}", user_input.trim());
+    io::stdin()
+        .read_line(&mut user_input_buffer)
+        .expect("Failed to read user input!");
 
+    let user_input: f64 = user_input_buffer
+        .trim()
+        .parse()
+        .expect("That's not a number!");
+
+    println!("You entered: {}", user_input);
 }
