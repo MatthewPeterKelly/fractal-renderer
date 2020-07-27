0af8d59659e81eda36e8a1d27af09b6c8d86ff2b
Matt Kelly <matthew.kelly2@gmail.com>
2020-07-26 20:02:23 -0400

    added mandelbrot_update

--- src/lib.rs
+++ src/lib.rs
@@ -32,10 +32,19 @@ pub mod mandelbrot_set {
         pub imag: f64,
     }
 
+    impl Complex {
+        pub fn mandelbrot_update(&mut self, c: &Complex) -> () {
+            let temp = self.real * self.real - self.imag * self.imag + c.real;
+            self.imag = 2.0 * self.real * self.imag + c.imag;
+            self.real = temp;
+        }
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn complex_constructor() {
         let complex = crate::mandelbrot_set::Complex {
@@ -44,6 +53,25 @@ mod tests {
         };
         assert_eq!(complex.real, 1.0);
         assert_eq!(complex.imag, 2.0);
+    }
+
+
+    #[test]
+    fn mandelbrot_update() {
+        let c = crate::mandelbrot_set::Complex {
+            real: 0.5,
+            imag: 0.0,
+        };
+        let mut z = crate::mandelbrot_set::Complex {
+            real: 0.0,
+            imag: 0.0,
+        };
+        z.mandelbrot_update(&c);
+        assert_eq!(z.real, 0.5);
+        assert_eq!(z.imag, 0.0);
+        z.mandelbrot_update(&c);
+        assert_eq!(z.real, 0.75);
+        assert_eq!(z.imag, 0.0);
     }
 
     #[test]
