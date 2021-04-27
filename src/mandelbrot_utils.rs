006cfee6b24d90deb0a5a82f043dffb644aa3a17
Matt Kelly <matthew.kelly2@gmail.com>
2021-04-27 09:16:58 -0400

    remove unused code

--- src/mandelbrot_utils.rs
+++ src/mandelbrot_utils.rs
@@ -1,20 +1,6 @@
 use crate::pixel_iter::Point2d;
 
 #[derive(Debug, Copy, Clone)]
-pub struct Complex {
-    pub real: f64,
-    pub imag: f64,
-}
-
-impl Complex {
-    pub fn mandelbrot_update(&mut self, c: &Complex) -> () {
-        let temp = self.real * self.real - self.imag * self.imag + c.real;
-        self.imag = 2.0 * self.real * self.imag + c.imag;
-        self.real = temp;
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
 pub struct MandelbrotResult {
     pub value: f64, // scaled betweeen 0 and 1, possibly interpolated
     pub count: u32, // integer number of iterations, saturated
@@ -41,34 +27,6 @@ fn compute_mandelbrot(c: &Point2d, max_iter: u32) -> MandelbrotResult {
 
 #[cfg(test)]
 mod tests {
-
-    #[test]
-    fn complex_constructor() {
-        let complex = crate::mandelbrot_utils::Complex {
-            real: 1.0,
-            imag: 2.0,
-        };
-        assert_eq!(complex.real, 1.0);
-        assert_eq!(complex.imag, 2.0);
-    }
-
-    #[test]
-    fn mandelbrot_update() {
-        let c = crate::mandelbrot_utils::Complex {
-            real: 0.5,
-            imag: 0.0,
-        };
-        let mut z = crate::mandelbrot_utils::Complex {
-            real: 0.0,
-            imag: 0.0,
-        };
-        z.mandelbrot_update(&c);
-        assert_eq!(z.real, 0.5);
-        assert_eq!(z.imag, 0.0);
-        z.mandelbrot_update(&c);
-        assert_eq!(z.real, 0.75);
-        assert_eq!(z.imag, 0.0);
-    }
 
     #[test]
     fn compute_mandelbrot_test() {
