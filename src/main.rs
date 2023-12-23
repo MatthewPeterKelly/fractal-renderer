e18dbf68920508b4c99705c80077b50511f4f25b
Matthew Kelly <matthew.kelly2@gmail.com>
2023-12-23 11:18:11 -0500

    clean up main file

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ fn main() {
 
     match &args.command {
         Some(CommandsEnum::MandelbrotRender(params)) => {
-            let params_json =
-                std::fs::read_to_string(&params.params_path).expect("Unable to read param file");
-            let params: crate::mandelbrot_core::MandelbrotParams =
-                serde_json::from_str(&params_json).unwrap();
             crate::mandelbrot_core::render_mandelbrot_set(
-                &params,
+                &serde_json::from_str(
+                    &std::fs::read_to_string(&params.params_path)
+                        .expect("Unable to read param file"),
+                )
+                .unwrap(),
                 &crate::file_io::build_output_path("mandelbrot_render"),
                 "render",
             )
@@ -25,15 +25,12 @@ fn main() {
         }
 
         Some(CommandsEnum::MandelbrotSearch(params)) => {
-            // Load parameters for each individual render:
-            println!("Params file: {}", params.params_path);
-            let render_params_str =
-                std::fs::read_to_string(&params.params_path).expect("Unable to read param file");
-            let params: crate::mandelbrot_search::MandelbrotSearchParams =
-                serde_json::from_str(&render_params_str).unwrap();
-
             crate::mandelbrot_search::mandelbrot_search_render(
-                &params,
+                &serde_json::from_str(
+                    &std::fs::read_to_string(&params.params_path)
+                        .expect("Unable to read param file"),
+                )
+                .unwrap(),
                 &crate::file_io::build_output_path("mandelbrot_search"),
             )
             .unwrap();
