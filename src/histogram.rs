01cbeea67bf84a1d82ae44cd12b426469a077436
Matthew Kelly <matthew.kelly2@gmail.com>
2024-02-15 19:51:37 -0500

    Add cumulative distribution function (#21)
    Progress towards coloring the Mandelbrot using a histogram: 
    https://github.com/MatthewPeterKelly/fractal-renderer/issues/15.
    
    Adds a fast and simple implementation of a cumulative distribution
    function, which is constructed from a histogram. Like the histogram,
    it is optimized for fast runtime, given that it will be evaluated for
    every point in the image.

--- src/histogram.rs
+++ src/histogram.rs
@@ -2,18 +2,23 @@ use std::io::{self, Write};
 
 pub struct Histogram {
     pub bin_count: Vec<u32>,
-    pub value_to_index_scale: f64,
+    pub data_to_index_scale: f64,
+    pub bin_width: f64,
 }
 
+/**
+ * Fast and simple histogram for non-negative data.
+ */
 impl Histogram {
     // Constructor
     pub fn new(num_bins: usize, max_val: f64) -> Self {
         assert!(num_bins > 0, "`num_bins` must be positive!");
         assert!(max_val > 0.0, "`max_val` must be positive!");
-        let value_to_index_scale = (num_bins as f64) / max_val;
+        let data_to_index_scale = (num_bins as f64) / max_val;
         Histogram {
             bin_count: vec![0; num_bins],
-            value_to_index_scale,
+            data_to_index_scale,
+            bin_width: 1.0 / data_to_index_scale,
         }
     }
 
@@ -23,7 +28,7 @@ impl Histogram {
             self.bin_count[0] += 1;
             return;
         }
-        let index = (data * self.value_to_index_scale) as usize;
+        let index = (data * self.data_to_index_scale) as usize;
         if index >= self.bin_count.len() {
             *self.bin_count.last_mut().unwrap() += 1;
         } else {
@@ -35,22 +40,100 @@ impl Histogram {
         self.bin_count.iter().sum()
     }
 
-    // Print the histogram
+    /**
+     * @return: the lower edge of the specified bin (inclusive)
+     */
+    pub fn lower_edge(&self, bin_index: usize) -> f64 {
+        self.bin_width * (bin_index as f64)
+    }
+
+    /**
+     * @return: the upper edge of the specified bin (exclusive)
+     */
+    pub fn upper_edge(&self, bin_index: usize) -> f64 {
+        self.bin_width * ((bin_index + 1) as f64)
+    }
+
+    /**
+     * Print the histogram stats to the writer
+     */
     pub fn display<W: Write>(&self, mut writer: W) -> io::Result<()> {
         let total = self.total_count();
         let percent_scale = 100.0 / (total as f64);
         writeln!(writer, "total count: {}", total)?;
         for i in 0..self.bin_count.len() {
-            let low_bnd = self.value_to_index_scale * (i as f64);
-            let upp_bnd = self.value_to_index_scale * ((i + 1) as f64);
             let count = self.bin_count[i];
             let percent = (count as f64) * percent_scale;
             writeln!(
                 writer,
                 "bins[{}]:  [{:.2}, {:.2}) --> {}  ({:.2}%)",
-                i, low_bnd, upp_bnd, count, percent
+                i,
+                self.lower_edge(i),
+                self.upper_edge(i),
+                self.bin_count[i],
+                percent
             )?;
         }
         Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct CumulativeDistributionFunction {
+    pub offset: Vec<f64>, // n_bins
+    pub scale: Vec<f64>,  // n_bins
+    pub data_to_index_scale: f64,
+    pub min_data: f64, // --> maps to 0.0
+    pub max_data: f64, // --> maps to 1.0
+}
+
+impl CumulativeDistributionFunction {
+    pub fn new(histogram: Histogram) -> CumulativeDistributionFunction {
+        let scale_bin_count_to_fraction = 1.0 / (histogram.total_count() as f64);
+
+        let n_bins = histogram.bin_count.len();
+        let mut offset: Vec<f64> = Vec::with_capacity(n_bins);
+        let mut scale: Vec<f64> = Vec::with_capacity(n_bins);
+        let mut accumulated_count = 0;
+
+        // x = data (input)
+        // y = value (output, fraction within population)
+        let mut y_low = 0.0;
+        for i in 0..histogram.bin_count.len() {
+            accumulated_count += histogram.bin_count[i];
+            let y_upp = (accumulated_count as f64) * scale_bin_count_to_fraction;
+            let x_low = histogram.lower_edge(i);
+            let dy_dx = (y_upp - y_low) * histogram.data_to_index_scale;
+
+            offset.push(y_low - x_low * dy_dx);
+            scale.push(dy_dx);
+
+            y_low = y_upp; // for the next iteration
+        }
+
+        CumulativeDistributionFunction {
+            offset,
+            scale,
+            data_to_index_scale: histogram.data_to_index_scale,
+            min_data: histogram.lower_edge(0),
+            max_data: histogram.upper_edge(n_bins - 1),
+        }
+    }
+
+    /**
+     * @param value: data point, same units as would be used in the histogram
+     * @return: fractional position within the population of the histogram
+     */
+    pub fn percentile(&self, data: f64) -> f64 {
+        if data <= self.min_data {
+            return 0.0;
+        }
+        if data >= self.max_data {
+            return 1.0;
+        }
+        // Interesting case: linearly interpolate between edges.
+        // Interpolating coefficients are pre-computed in the constructor
+        let index = (data * self.data_to_index_scale) as usize;
+        self.offset[index] + data * self.scale[index]
     }
 }
--- tests/histogram.rs
+++ tests/histogram.rs
@@ -2,10 +2,11 @@
 mod tests {
     use std::{fs, io};
 
-    use fractal_renderer::histogram::Histogram;
+    use approx::assert_relative_eq;
+    use fractal_renderer::histogram::{CumulativeDistributionFunction, Histogram};
 
     #[test]
-    fn test_insert_positive_data() {
+    fn test_histogram_insert_positive_data() {
         let mut hist = Histogram::new(5, 10.0);
 
         hist.insert(2.5);
@@ -15,7 +16,7 @@ mod tests {
     }
 
     #[test]
-    fn test_insert_negative_data() {
+    fn test_histogram_insert_negative_data() {
         let mut hist = Histogram::new(5, 10.0);
 
         hist.insert(-3.0);
@@ -25,7 +26,7 @@ mod tests {
     }
 
     #[test]
-    fn test_insert_data_at_max_val() {
+    fn test_histogram_insert_data_at_max_val() {
         let mut hist = Histogram::new(5, 10.0);
 
         hist.insert(10.0);
@@ -34,7 +35,7 @@ mod tests {
     }
 
     #[test]
-    fn test_insert_data_greater_than_max_val() {
+    fn test_histogram_insert_data_greater_than_max_val() {
         let mut hist = Histogram::new(5, 10.0);
 
         hist.insert(12.5);
@@ -43,19 +44,19 @@ mod tests {
     }
 
     #[test]
-    fn test_insert_with_zero_num_bins() {
+    fn test_histogram_insert_with_zero_num_bins() {
         // This should panic due to the assertion in the constructor
         assert!(std::panic::catch_unwind(|| Histogram::new(0, 10.0)).is_err());
     }
 
     #[test]
-    fn test_insert_with_zero_max_val() {
+    fn test_histogram_insert_with_zero_max_val() {
         // This should panic due to the assertion in the constructor
         assert!(std::panic::catch_unwind(|| Histogram::new(5, 0.0)).is_err());
     }
 
     #[test]
-    fn test_text_display() {
+    fn test_histogram_text_display() {
         let mut hist = Histogram::new(3, 4.0);
         hist.insert(0.3);
         hist.insert(2.3);
@@ -66,7 +67,7 @@ mod tests {
     }
 
     #[test]
-    fn test_file_display() {
+    fn test_histogram_file_display() {
         let mut hist = Histogram::new(3, 9.0);
         hist.insert(0.3);
         hist.insert(1.3);
@@ -77,5 +78,102 @@ mod tests {
             .expect("failed to create `histogram_test_file_display.txt`");
         let buf_writer = io::BufWriter::new(file);
         hist.display(buf_writer).expect("Failed to write to file");
+    }
+
+    #[test]
+    fn test_histogram_utilities() {
+        let mut hist = Histogram::new(3, 6.0);
+        hist.insert(0.3);
+        hist.insert(1.3);
+        hist.insert(2.6);
+        hist.insert(0.2);
+
+        assert_eq!(hist.total_count(), 4);
+
+        let tol = 1e-6;
+
+        assert_relative_eq!(hist.lower_edge(0), 0.0, epsilon = tol);
+        assert_relative_eq!(hist.upper_edge(0), 2.0, epsilon = tol);
+        assert_relative_eq!(hist.lower_edge(1), 2.0, epsilon = tol);
+        assert_relative_eq!(hist.upper_edge(1), 4.0, epsilon = tol);
+        assert_relative_eq!(hist.lower_edge(2), 4.0, epsilon = tol);
+        assert_relative_eq!(hist.upper_edge(2), 6.0, epsilon = tol);
+    }
+
+    #[test]
+    fn test_cdf_uniform() {
+        let max_value = 6.0;
+        let mut hist = Histogram::new(3, max_value);
+        hist.insert(1.3);
+        hist.insert(2.6);
+        hist.insert(4.2);
+        let cdf = CumulativeDistributionFunction::new(hist);
+
+        let tol = 1e-6;
+
+        // out-of-bounds checks:
+        assert_eq!(cdf.percentile(-0.2), 0.0);
+        assert_eq!(cdf.percentile(7.0), 1.0);
+
+        // the CFG for a uniform histogram is linear
+        for data in iter_num_tools::lin_space(0.0..=max_value, 17) {
+            assert_relative_eq!(cdf.percentile(data), data / max_value, epsilon = tol);
+        }
+    }
+
+    #[test]
+    fn test_cdf_skewed() {
+        let mut hist = Histogram::new(3, 6.0);
+        hist.insert(4.7);
+        hist.insert(5.2);
+        hist.insert(4.2);
+        hist.insert(4.2);
+        let cdf = CumulativeDistributionFunction::new(hist);
+
+        let tol = 1e-6;
+
+        // check emtpy bins --> 0
+        assert_eq!(cdf.percentile(1.0), 0.0);
+        assert_eq!(cdf.percentile(3.0), 0.0);
+
+        // edge of the first useful data point:
+        assert_eq!(cdf.percentile(4.0), 0.0);
+
+        // now its linear:
+        assert_relative_eq!(cdf.percentile(4.1), 0.05, epsilon = tol);
+        assert_relative_eq!(cdf.percentile(5.0), 0.5, epsilon = tol);
+        assert_relative_eq!(cdf.percentile(5.9), 0.95, epsilon = tol);
+
+        // upper bound
+        assert_relative_eq!(cdf.percentile(6.0), 1.0, epsilon = tol);
+    }
+
+    #[test]
+    fn test_cdf_interesting() {
+        let mut hist = Histogram::new(5, 25.0);
+        for _ in 0..3 {
+            hist.insert(3.0);
+        }
+        for _ in 0..9 {
+            hist.insert(12.0);
+        }
+        for _ in 0..12 {
+            hist.insert(24.0);
+        }
+        let cdf = CumulativeDistributionFunction::new(hist);
+
+        // edges
+        assert_eq!(cdf.percentile(0.0), 0.0);
+        assert_eq!(cdf.percentile(25.0), 1.0);
+
+        // constant region
+        assert_eq!(cdf.percentile(5.0), 0.125);
+        assert_eq!(cdf.percentile(7.0), 0.125);
+        assert_eq!(cdf.percentile(10.0), 0.125);
+
+        // constant region
+        assert_eq!(cdf.percentile(15.0), 0.5);
+        assert_eq!(cdf.percentile(17.0), 0.5);
+        assert_eq!(cdf.percentile(20.0), 0.5);
     }
 }
