b5d871eb2ba76ccb8334a872c4a7d41fdd3b180b
Matthew Kelly <matthew.kelly2@gmail.com>
2024-03-07 21:15:25 -0500

    clean up

--- src/cli.rs
+++ src/cli.rs
@@ -18,16 +18,6 @@ pub struct ParameterFilePath {
     pub params_path: String,
     #[clap(long, short)]
     pub date_time_out: bool,
-    #[clap(long, short)]
+    #[clap(long, short, default_value = "1.0")]
     pub rescale: f64,
-}
-
-impl Default for ParameterFilePath {
-    fn default() -> Self {
-        ParameterFilePath {
-            params_path: String::default(),
-            date_time_out: false,
-            rescale: 1.0,
-        }
-    }
 }
