6ef221b4f038fcc416e93b946f48becf7f541f6c
Matthew Kelly <matthew.kelly2@gmail.com>
2024-06-23 07:58:47 -0400

    generalize the contraction ratio calculation

--- src/serpinsky.rs
+++ src/serpinsky.rs
@@ -26,10 +26,28 @@ fn polygon_verticies(num_vertices: usize) -> Vec<nalgebra::Vector2<f64>> {
     vertices
 }
 
+// Optimal "jump fraction" for each sample.
+// Based on: https://en.wikipedia.org/wiki/Chaos_game#cite_note-6
+// Abdulaziz, Abdulrahman; Said, Judy (September 2021).
+// "On the contraction ratio of iterated function systems whose attractors are Sierpinski n -gons".
+// Chaos, Solitons & Fractals. 150: 111140.
+fn optimal_contraction_ratio(n: usize) -> f64 {
+    let n_mod_4 = n % 4;
+    use std::f64::consts::PI;
+    let alpha = match n_mod_4 {
+        0 => (PI / n as f64).tan(),
+        1 | 3 => 2.0 * (PI / (2 * n) as f64).sin(),
+        2 => (PI / n as f64).sin(),
+        _ => unreachable!(), // This case will never occur
+    };
+    1.0 / (1.0 + alpha)
+}
+
 struct SampleGenerator {
     distribution: Uniform<usize>, // which vertext to jump to?
     vertices: Vec<nalgebra::Vector2<f64>>,
     colors: Vec<image::Rgba<u8>>,
+    ratio: f64,
 }
 
 impl SampleGenerator {
@@ -41,20 +59,19 @@ impl SampleGenerator {
                 .iter()
                 .map(|&color| image::Rgba(color))
                 .collect(),
+            ratio: optimal_contraction_ratio(vertex_colors.len()),
         }
     }
 
     pub fn next<R: Rng>(
         &self,
         rng: &mut R,
+
         prev_sample: &nalgebra::Vector2<f64>,
     ) -> chaos_game::ColoredPoint {
         let vertex_index = self.distribution.sample(rng);
-        // Optional alpha reference: https://en.wikipedia.org/wiki/Chaos_game
-        let alpha = 0.5; // Implicitly depends on hard-coded 3-side assumption
-
         let selected_vertex = self.vertices[vertex_index];
-        let next_point = alpha * selected_vertex + (1.0 - alpha) * prev_sample;
+        let next_point = self.ratio * selected_vertex + (1.0 - self.ratio) * prev_sample;
         let next_color = self.colors[vertex_index];
         chaos_game::ColoredPoint {
             point: next_point,
@@ -91,4 +108,22 @@ pub fn render_serpinsky(
         file_prefix,
         &serde_json::to_string(params)?,
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::serpinsky::optimal_contraction_ratio;
+    use approx::assert_relative_eq;
+
+    #[test]
+    fn test_optimal_packing_ratio() {
+        // Solutions from: https://en.wikipedia.org/wiki/Chaos_game
+        let tol = 0.005;
+        assert_relative_eq!(optimal_contraction_ratio(5), 0.618, epsilon = tol);
+        assert_relative_eq!(optimal_contraction_ratio(6), 0.667, epsilon = tol);
+        assert_relative_eq!(optimal_contraction_ratio(7), 0.692, epsilon = tol);
+        assert_relative_eq!(optimal_contraction_ratio(8), 0.707, epsilon = tol);
+        assert_relative_eq!(optimal_contraction_ratio(9), 0.742, epsilon = tol);
+        assert_relative_eq!(optimal_contraction_ratio(10), 0.764, epsilon = tol);
+    }
 }
