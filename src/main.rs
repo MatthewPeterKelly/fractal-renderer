be204e3be86509820384d88a54335339736994d2
Matthew Kelly <matthew.kelly2@gmail.com>
2024-07-13 11:36:00 -0400

    Fixed directory naming hack

--- src/main.rs
+++ src/main.rs
@@ -35,26 +35,35 @@ fn build_params(cli_params: &cli::ParameterFilePath) -> mandelbrot_core::Mandelb
     mandel_params
 }
 
-pub fn main_render(
+pub fn main_render<F>(
     // TODO:  fix namespacing
     params: &RenderParams,
-    file_prefix: &file_io::FilePrefix,
-) -> Result<(), Box<dyn std::error::Error>> {
+    file_prefix: F,
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    F: Fn(&str) -> file_io::FilePrefix,
+{
     match params {
         RenderParams::Mandelbrot(inner_params) => {
-            crate::mandelbrot_core::render_mandelbrot_set(inner_params, file_prefix)
+            crate::mandelbrot_core::render_mandelbrot_set(inner_params, &file_prefix("mendelbrot"))
         }
         RenderParams::MandelbrotSearch(inner_params) => {
-            crate::mandelbrot_search::mandelbrot_search_render(inner_params, file_prefix)
+            crate::mandelbrot_search::mandelbrot_search_render(
+                inner_params,
+                &file_prefix("mandelbrot_search"),
+            )
         }
         RenderParams::DrivenDampedPendulum(inner_params) => {
-            crate::ddp_utils::render_driven_damped_pendulum_attractor(inner_params, file_prefix)
+            crate::ddp_utils::render_driven_damped_pendulum_attractor(
+                inner_params,
+                &file_prefix("DDP"),
+            )
         }
         RenderParams::BarnsleyFern(inner_params) => {
-            crate::barnsley_fern::render_barnsley_fern(inner_params, file_prefix)
+            crate::barnsley_fern::render_barnsley_fern(inner_params, &file_prefix("barnsley_fern"))
         }
         RenderParams::Serpinsky(inner_params) => {
-            crate::serpinsky::render_serpinsky(inner_params, file_prefix)
+            crate::serpinsky::render_serpinsky(inner_params, &file_prefix("serpinsky"))
         }
     }
 }
@@ -137,18 +146,22 @@ fn main() {
         }
 
         Some(CommandsEnum::Render(params)) => {
+            let build_file_prefix = |base_name: &str| -> file_io::FilePrefix {
+                file_io::FilePrefix {
+                    directory_path: crate::file_io::build_output_path_with_date_time(
+                        params, base_name, &datetime,
+                    ),
+                    file_base: file_io::extract_base_name(&params.params_path).to_owned(),
+                }
+            };
+
             main_render(
                 &serde_json::from_str(
                     &std::fs::read_to_string(&params.params_path)
                         .expect("Unable to read param file"),
                 )
                 .unwrap(),
-                &file_io::FilePrefix {
-                    directory_path: crate::file_io::build_output_path_with_date_time(
-                        params, "render", &datetime, // TODO:  pass correct base name?
-                    ),
-                    file_base: file_io::extract_base_name(&params.params_path).to_owned(),
-                },
+                build_file_prefix,
             )
             .unwrap();
         }
