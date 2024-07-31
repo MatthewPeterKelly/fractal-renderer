35e43416c33d6cd3b43cde07b22b8d6c7f32aab9
Matthew Kelly <matthew.kelly2@gmail.com>
2024-07-31 14:08:36 -0400

    WIP:  initial implementation

--- src/core/color_map.rs
+++ src/core/color_map.rs
@@ -1,3 +1,6 @@
+use palette::{Mix, Srgb};
+
+
 /**
 
 // Nice color map and framing here:
@@ -32,3 +35,91 @@ To convert these to RGB, we'll decode each hex color value into its respective
 
  *
  */
+
+/**
+ * TODO:  docs
+ */
+pub struct ColorMapKeyFrame {
+    pub query: f32,       // specify location of this color within the map; on [0,1]
+    pub rgb_raw: [u8; 3], // [R, G, B]
+}
+
+/**
+ * TODO:  docs
+ */
+pub struct PiecewiseLinearColorMap {
+    keyframes: Vec<ColorMapKeyFrame>,
+}
+
+impl PiecewiseLinearColorMap {
+    // TODO:  docs
+    // TODO:  better error messages
+    pub fn new(keyframes: Vec<ColorMapKeyFrame>) -> PiecewiseLinearColorMap {
+        if keyframes.is_empty() {
+            println!("ERROR:  keyframes are empty!");
+            panic!();
+        }
+
+        if keyframes.first().unwrap().query != 0.0 {
+            println!("ERROR:  initial keyframe query point must be 0.0!");
+            panic!();
+        }
+        if keyframes.last().unwrap().query != 1.0 {
+            println!("ERROR:  final keyframe query point must be 1.0!");
+            panic!();
+        }
+
+        for i in 0..(keyframes.len() - 1) {
+            if keyframes[i].query >= keyframes[i + 1].query {
+                println!("ERROR:  keyframes should be monotonic, but are not!");
+                panic!();
+            }
+        }
+        PiecewiseLinearColorMap { keyframes }
+    }
+
+    pub fn compute(&self, query: f32) -> [u8; 3] {
+        if query <= 0.0f32 {
+            self.keyframes.first().unwrap().rgb_raw
+        } else if query >= 1.0f32 {
+            self.keyframes.last().unwrap().rgb_raw
+        } else {
+            let (i, j) = self.linear_index_search(query);
+            let alpha = (query - self.keyframes[i].query)
+                / (self.keyframes[j].query - self.keyframes[i].query);
+            Self::srgb_interpolate(&self.keyframes[i].rgb_raw, &self.keyframes[j].rgb_raw, alpha)
+        }
+    }
+    fn linear_index_search(&self, query: f32) -> (usize, usize) {
+        let mut idx_low = self.keyframes.len() / 2;
+
+        // hard limit on upper iteration, to catch bugs
+        for _ in 0..self.keyframes.len() {
+            if query < self.keyframes[idx_low].query {
+                idx_low -= 1;
+                continue;
+            }
+            if query >= self.keyframes[idx_low + 1].query {
+                idx_low += 1;
+                continue;
+            }
+            // [low <= query < upp]  --> success!
+            return (idx_low, idx_low + 1);
+        }
+
+        println!("ERROR:  Linear keyframe search failed!");
+        panic!();
+    }
+
+    fn srgb_interpolate(low: &[u8; 3], upp: &[u8; 3], alpha: f32) -> [u8; 3] {
+        // Convert low and upp from [u8; 3] to Srgb using from_format
+        let low_srgb = Srgb::from_format((*low).into());
+        let upp_srgb = Srgb::from_format((*upp).into());
+
+        // Interpolate between the two colors in the sRGB color space
+        let interp_srgb = low_srgb.mix(upp_srgb, alpha);
+
+        // Convert back to [u8; 3] using into_format
+        interp_srgb.into_format().into()
+    }
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,4 +1,5 @@
 pub mod chaos_game;
+pub mod color_map;
 pub mod file_io;
 pub mod histogram;
 pub mod image_utils;
