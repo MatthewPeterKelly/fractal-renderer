d7ca8af579018969954d5c8c3cd94ee7324ed35e
Matthew Kelly <matthew.kelly2@gmail.com>
2024-08-18 13:48:21 -0400

    Add timing diagnostic for color color_swatch

--- src/cli/color_swatch.rs
+++ src/cli/color_swatch.rs
@@ -7,6 +7,7 @@ use crate::core::{
     },
     file_io::{serialize_to_json_or_panic, FilePrefix},
     image_utils::write_image_to_file_or_panic,
+    stopwatch::{self, Stopwatch},
 };
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -34,6 +35,8 @@ pub fn generate_color_swatch(params_path: &str, file_prefix: FilePrefix) {
         &std::fs::read_to_string(params_path).expect("Unable to read param file"),
     )
     .unwrap();
+
+    let mut stopwatch = Stopwatch::new("Color Swatch".to_owned());
 
     serialize_to_json_or_panic(file_prefix.full_path_with_suffix(".json"), &params);
 
@@ -63,6 +66,8 @@ pub fn generate_color_swatch(params_path: &str, file_prefix: FilePrefix) {
         },
     ];
 
+    stopwatch.record_split("setup color maps".to_owned());
+
     // Save the image to a file, deducing the type from the file name
     // Create a new ImgBuf to store the render in memory (and eventually write it to a file).
     let mut imgbuf = {
@@ -77,18 +82,29 @@ pub fn generate_color_swatch(params_path: &str, file_prefix: FilePrefix) {
     let mut y_offset = params.border_padding;
     let scale = 1.0 / ((params.swatch_resolution.0 * params.swatch_resolution.1) as f32);
 
+    stopwatch.record_split("setup image buffer".to_owned());
+
     for named_map in color_maps {
         for x_idx in 0..params.swatch_resolution.0 {
             for y_idx in 0..params.swatch_resolution.1 {
                 let linear_index = x_idx * params.swatch_resolution.1 + y_idx;
-                *imgbuf.get_pixel_mut(x_idx + x_offset, y_idx + y_offset) =
-                    named_map.color_map.compute_pixel(scale * (linear_index as f32));
+                *imgbuf.get_pixel_mut(x_idx + x_offset, y_idx + y_offset) = named_map
+                    .color_map
+                    .compute_pixel(scale * (linear_index as f32));
             }
         }
         y_offset += params.swatch_resolution.1 + params.border_padding;
+
+        stopwatch.record_split(format!("evaluate color map:  {}", named_map.name.clone()));
     }
 
     write_image_to_file_or_panic(file_prefix.full_path_with_suffix(".png"), |f| {
         imgbuf.save(f)
     });
+
+    stopwatch.record_split("write image file".to_owned());
+
+    stopwatch
+        .display(&mut file_prefix.create_file_with_suffix("_diagnostics.txt"))
+        .unwrap();
 }
