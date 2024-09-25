89d98779d1b11370a16f67942e6a8cfaf4223b8a
Matthew Kelly <matthew.kelly2@gmail.com>
2024-09-25 05:47:00 -0400

    Test passing, but needs clean-up

--- tests/example_parameter_validation_tests.rs
+++ tests/example_parameter_validation_tests.rs
@@ -1,17 +1,17 @@
 #[cfg(test)]
 mod tests {
     use std::fs;
-    use fractal_renderer::fractals::common::FractalParams;
+    use fractal_renderer::{cli::color_swatch::ColorSwatchParams, fractals::common::FractalParams};
     use glob::glob;
+    use serde::de::DeserializeOwned;
 
 
-    #[test]
-    fn test_parse_all_json_files() {
-        // Define the directory containing your JSON files
-        let dir = "examples"; // Update this path
-
+    fn test_parse_parameter_file<T>(directory: &str)
+    where
+        T: DeserializeOwned,
+    {
         // Create a pattern to match all .json files in the directory
-        let pattern = format!("{}/**/*.json", dir);
+        let pattern = format!("{}/**/*.json", directory);
 
         // Use glob to find all matching .json files
         for entry in glob(&pattern).expect("Failed to read glob pattern") {
@@ -21,8 +21,8 @@ mod tests {
                     let content = fs::read_to_string(&path)
                         .unwrap_or_else(|_| panic!("Failed to read file: {:?}", path));
 
-                    // Attempt to parse the JSON file into your struct
-                    let result: Result<FractalParams, _> = serde_json::from_str(&content);
+                    // Attempt to parse the JSON file into the specified type
+                    let result: Result<T, _> = serde_json::from_str(&content);
 
                     // Assert that the parsing is successful
                     assert!(
@@ -35,5 +35,15 @@ mod tests {
                 Err(e) => panic!("Failed to read path: {:?}", e),
             }
         }
+    }
+
+
+    #[test]
+    fn test_ensure_all_example_files_can_be_parsed() {
+        test_parse_parameter_file::<FractalParams>("examples/barnsley_fern");
+        test_parse_parameter_file::<FractalParams>("examples/driven_damped_pendulum");
+        test_parse_parameter_file::<FractalParams>("examples/mandelbrot");
+        test_parse_parameter_file::<FractalParams>("examples/serpinsky");
+        test_parse_parameter_file::<ColorSwatchParams>("examples/color_swatch");
     }
 }
