22f7c519603204e67e76716730c051354237896e
Matthew Kelly <matthew.kelly2@gmail.com>
2024-09-25 05:57:59 -0400

    Improve error message

--- tests/example_parameter_validation_tests.rs
+++ tests/example_parameter_validation_tests.rs
@@ -4,35 +4,32 @@ mod tests {
     use fractal_renderer::{cli::color_swatch::ColorSwatchParams, fractals::common::FractalParams};
     use glob::glob;
     use serde::de::DeserializeOwned;
+    use std::any::type_name;
 
 
-    fn test_parse_parameter_file<T>(directory: &str)
-    where
-        T: DeserializeOwned,
+    fn test_parse_parameter_file<T: DeserializeOwned>(directory: &str)
     {
-        // Create a pattern to match all .json files in the directory
         let pattern = format!("{}/**/*.json", directory);
 
         // Use glob to find all matching .json files
+        // For each match, ensure that we can (1) open the file and (2) parse it into the specified parameter type.
         for entry in glob(&pattern).expect("Failed to read glob pattern") {
             match entry {
                 Ok(path) => {
-                    // Read the file content
                     let content = fs::read_to_string(&path)
                         .unwrap_or_else(|_| panic!("Failed to read file: {:?}", path));
 
-                    // Attempt to parse the JSON file into the specified type
                     let result: Result<T, _> = serde_json::from_str(&content);
 
-                    // Assert that the parsing is successful
                     assert!(
                         result.is_ok(),
-                        "Failed to parse JSON file: {:?}, Error: {:?}",
+                        "Failed to parse JSON file: {:?} as parameter type: `{}`. Error:\n\n{:?}\n\n",
                         path,
+                        type_name::<T>(),
                         result.err()
                     );
                 }
-                Err(e) => panic!("Failed to read path: {:?}", e),
+                Err(e) => panic!("Failed to read path: {:?}. Check permissions.", e),
             }
         }
     }
@@ -40,10 +37,13 @@ mod tests {
 
     #[test]
     fn test_ensure_all_example_files_can_be_parsed() {
-        test_parse_parameter_file::<FractalParams>("examples/barnsley_fern");
-        test_parse_parameter_file::<FractalParams>("examples/driven_damped_pendulum");
-        test_parse_parameter_file::<FractalParams>("examples/mandelbrot");
-        test_parse_parameter_file::<FractalParams>("examples/serpinsky");
+        for sub_dir in ["/barnsley_fern",
+            "/driven_damped_pendulum",
+            "/mandelbrot",
+            "/serpinsky"] {
+                test_parse_parameter_file::<FractalParams>(&format!("examples/{}", sub_dir));
+            }
+
         test_parse_parameter_file::<ColorSwatchParams>("examples/color_swatch");
     }
 }
