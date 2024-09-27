6285b74b727e2b41831920bfb41436b0ae407688
Matthew Kelly <matthew.kelly2@gmail.com>
2024-09-27 06:40:44 -0400

    WIP:  full check on file hash

--- tests/full_cli_integration_and_regression_tests.rs
+++ tests/full_cli_integration_and_regression_tests.rs
@@ -1,5 +1,43 @@
 use std::process::Command;
 
+use sha2::{Digest, Sha256};
+use std::fs::File;
+use std::io::{self, Read};
+
+fn compute_file_hash(file_path: &str) -> Result<String, io::Error> {
+    let mut file = File::open(file_path)?;
+    let mut hasher = Sha256::new();
+    let mut buffer = Vec::new();
+
+    file.read_to_end(&mut buffer)?;
+
+    hasher.update(&buffer);
+
+    let result = hasher.finalize();
+    Ok(format!("{:x}", result))
+}
+
+fn check_file_hash(file_path: &str, expected_hash: &str) ->bool {
+    match compute_file_hash(file_path) {
+        Ok(computed_hash) => {
+            if computed_hash == expected_hash {
+                println!("Hash matches the expected value.");
+                return true;
+            } else {
+                println!(
+                    "Hash mismatch! Expected: {}, but got: {}",
+                    expected_hash, computed_hash
+                );
+                return false;
+            }
+        }
+        Err(e) => {
+            eprintln!("Error reading the file: {:?}", e);
+            return false;
+        }
+    }
+}
+
 fn run_command(command: &str, args: &[&str]) {
     let status = Command::new(command)
         .args(args)
@@ -9,10 +47,14 @@ fn run_command(command: &str, args: &[&str]) {
 }
 #[cfg(test)]
 mod tests {
-    use crate::run_command;
+    use crate::{check_file_hash, run_command};
 
 #[test]
 fn test_mandelbrot_render() {
     run_command("cargo", &["run", "--release", "--", "render", "./tests/param_files/mandelbrot_default_tiny.json"]);
+    let file_path = "out/render/mandelbrot/mandelbrot_default_tiny.png";
+    let expected_hash = "559ef8eadaeab60dc8136933acd8b9eb7c589e69263ec995a5e526ad79b9ec14";
+
+    assert!(check_file_hash(file_path, expected_hash));
 }
 }
