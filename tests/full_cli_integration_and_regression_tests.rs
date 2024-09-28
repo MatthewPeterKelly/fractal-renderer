aa6ab926788f755aa296d98e15ea15d3dbb3fb2e
Matthew Kelly <matthew.kelly2@gmail.com>
2024-09-28 05:46:52 -0400

    Make test helper function

--- tests/full_cli_integration_and_regression_tests.rs
+++ tests/full_cli_integration_and_regression_tests.rs
@@ -10,30 +10,28 @@ fn compute_file_hash(file_path: &str) -> Result<String, io::Error> {
     let mut buffer = Vec::new();
 
     file.read_to_end(&mut buffer)?;
-
     hasher.update(&buffer);
 
-    let result = hasher.finalize();
-    Ok(format!("{:x}", result))
+    Ok(format!("{:x}", hasher.finalize()))
 }
 
-fn check_file_hash(file_path: &str, expected_hash: &str) ->bool {
+fn check_file_hash(file_path: &str, expected_hash: &str) -> bool {
     match compute_file_hash(file_path) {
         Ok(computed_hash) => {
             if computed_hash == expected_hash {
                 println!("Hash matches the expected value.");
-                return true;
+                true
             } else {
                 println!(
                     "Hash mismatch! Expected: {}, but got: {}",
                     expected_hash, computed_hash
                 );
-                return false;
+                false
             }
         }
         Err(e) => {
             eprintln!("Error reading the file: {:?}", e);
-            return false;
+            false
         }
     }
 }
@@ -45,16 +43,33 @@ fn run_command(command: &str, args: &[&str]) {
         .expect("failed to execute process");
     assert!(status.success(), "Command {:?} failed", command);
 }
+
+fn render_image_and_verify_file_hash(test_param_file_name_base: &str, expected_hash: &str) -> bool {
+    run_command(
+        "cargo",
+        &[
+            "run",
+            "--release",
+            "--",
+            "render",
+            &format!("./tests/param_files/{}.json", test_param_file_name_base),
+        ],
+    );
+    let image_file_path = format!("out/render/{}.png", test_param_file_name_base);
+    check_file_hash(&image_file_path, expected_hash)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{check_file_hash, run_command};
+    use crate::render_image_and_verify_file_hash;
 
-#[test]
-fn test_mandelbrot_render() {
-    run_command("cargo", &["run", "--release", "--", "render", "./tests/param_files/mandelbrot_default_tiny.json"]);
-    let file_path = "out/render/mandelbrot/mandelbrot_default_tiny.png";
-    let expected_hash = "559ef8eadaeab60dc8136933acd8b9eb7c589e69263ec995a5e526ad79b9ec14";
+    #[test]
+    fn test_mandelbrot_render() {
+        let ok = render_image_and_verify_file_hash(
+            "mandelbrot/default_regression_test",
+            "559ef8eadaeab60dc8136933acd8b9eb7c589e69263ec995a5e526ad79b9ec14",
+        );
 
-    assert!(check_file_hash(file_path, expected_hash));
-}
+        assert!(ok);
+    }
 }
