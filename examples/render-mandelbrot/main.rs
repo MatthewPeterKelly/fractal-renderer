48b0e4e36b552a83f473f3d08d2f87d5598185a4
Matthew Kelly <matthew.kelly2@gmail.com>
2025-09-21 15:13:22 -0400

    Clean-up!  Examples tiny now.

--- examples/render-mandelbrot/main.rs
+++ examples/render-mandelbrot/main.rs
@@ -1,32 +1,9 @@
-use cli::args::ParameterFilePath;
-use fractal_renderer::{
-    cli::{self, render::render_fractal},
-    core::file_io::{build_output_path, FilePrefix},
-    fractals::common::FractalParams,
-};
+use fractal_renderer::cli::render::render_example_from_string;
 
 /// Run the default example for rendering the mandelbrot set.
 /// ```sh
 /// cargo run --example render-mandelbrot
 /// ```
 pub fn main() {
-    let example_name = "render-mandelbrot";
-
-    ////////////////
-
-    let params_name = String::from("examples/") + example_name + &String::from("/params.json");
-
-    let fractal_params = serde_json::from_str(
-        &std::fs::read_to_string(params_name).expect("Unable to read param file"),
-    )
-    .unwrap();
-
-    render_fractal(
-        &fractal_params,
-        FilePrefix {
-            directory_path: build_output_path(example_name),
-            file_base: String::from("result"),
-        },
-    )
-    .unwrap();
+    render_example_from_string("render-mandelbrot")
 }
--- src/cli/render.rs
+++ src/cli/render.rs
@@ -4,7 +4,27 @@ use crate::fractals::{
     barnsley_fern::render_barnsley_fern, common::FractalParams, serpinsky::render_serpinsky,
 };
 
-use crate::core::file_io::FilePrefix;
+use crate::core::file_io::{build_output_path, FilePrefix};
+
+// TODO:  crate-local?
+// Or move to examples?
+pub fn render_example_from_string(example_name: &str) {
+    let params_name = String::from("examples/") + example_name + &String::from("/params.json");
+
+    let fractal_params = serde_json::from_str(
+        &std::fs::read_to_string(params_name).expect("Unable to read param file"),
+    )
+    .unwrap();
+
+    render_fractal(
+        &fractal_params,
+        FilePrefix {
+            directory_path: build_output_path(example_name),
+            file_base: String::from("result"),
+        },
+    )
+    .unwrap();
+}
 
 pub fn render_fractal(
     params: &FractalParams,
