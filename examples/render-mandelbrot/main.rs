fed7742786213d2052db7dd59181d5d6fbb7e442
Matthew Kelly <matthew.kelly2@gmail.com>
2025-09-21 15:05:07 -0400

    clean-up.... WIP

--- examples/render-mandelbrot/main.rs
+++ examples/render-mandelbrot/main.rs
@@ -10,18 +10,14 @@ use fractal_renderer::{
 /// cargo run --example render-mandelbrot
 /// ```
 pub fn main() {
-    let fractal_params = |path: &str| -> FractalParams {
-        serde_json::from_str(&std::fs::read_to_string(path).expect("Unable to read param file"))
-            .unwrap()
-    };
-
-    let params = ParameterFilePath {
-        params_path: String::from("examples/render-mandelbrot/params.json"),
-        date_time_out: false,
-    };
+    let fractal_params = serde_json::from_str(
+        &std::fs::read_to_string(String::from("examples/render-mandelbrot/params.json"))
+            .expect("Unable to read param file"),
+    )
+    .unwrap();
 
     render_fractal(
-        &fractal_params(&params.params_path),
+        &fractal_params,
         FilePrefix {
             directory_path: build_output_path("examples"),
             file_base: String::from("mandelbrot"),
