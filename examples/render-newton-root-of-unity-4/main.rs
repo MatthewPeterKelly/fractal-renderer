537808e8ddbde79dd1e9f5f9bcf7c19e08a0a4d3
Matthew Kelly <matthew.kelly2@gmail.com>
2025-11-27 17:28:51 -0500

    Add an example for roots of unity

---
+++ examples/render-newton-root-of-unity-4/main.rs
@@ -0,0 +1,10 @@
+#[path = "../common/mod.rs"]
+mod common;
+
+/// Slightly more-expensive rendering of the Julia set, producing a flower-like pattern.
+/// ```sh
+/// cargo run --example render-julia-flower
+/// ```
+fn main() {
+    common::render_example_from_string("render-newton-root-of-unity-4")
+}
