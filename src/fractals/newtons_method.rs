928555e08ac3869f2b740864182a132b5f53f2cf
Matthew Kelly <matthew.kelly2@gmail.com>
2026-01-17 17:01:31 -0500

    updated!

--- src/fractals/newtons_method.rs
+++ src/fractals/newtons_method.rs
@@ -197,12 +197,14 @@ pub struct GrayscaleMapKeyFrame {
     pub value: f32,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct GrayscaleKeyframeSpec {
     pub root_colors_rgb: Vec<[u8; 3]>,
     pub grayscale_keyframes: Vec<GrayscaleMapKeyFrame>,
 }
 
 // TODO:  add some nice methods on this to turn it into a color map
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum ColorMapSpec {
     FullColorSpec(Vec<Vec<ColorMapKeyFrame>>),
     GrayscaleSpec(GrayscaleKeyframeSpec),
@@ -217,7 +219,7 @@ pub struct CommonParams {
     pub convergence_tolerance: f64,
     pub render_options: RenderOptions,
     pub boundary_set_color_rgb: [u8; 3],
-    pub cyclic_attractor_color_rgb: [u8; 3], // did not converge
+    pub cyclic_attractor_color_rgb: [u8; 3], // color for "did not converge"
     pub color_map_spec: ColorMapSpec,        // defines color map for each root
     pub lookup_table_count: usize,
     pub histogram_bin_count: usize,
@@ -251,36 +253,52 @@ pub struct NewtonsMethodRenderable<F: ComplexFunctionWithSlope> {
 
 impl<F: ComplexFunctionWithSlope> NewtonsMethodRenderable<F> {
     pub fn new(params: CommonParams, system: F) -> Self {
-        let mut inner_color_maps = Vec::new();
-        let mut color_maps = Vec::new();
-        for root_color in &params.root_colors_rgb {
-            // TODO:  call into a function that unpacks the keyframe spec into the color map keyframes
-            let keyframes: Vec<ColorMapKeyFrame> = params
-                .grayscale_keyframes
-                .iter()
-                .map(|keyframe| {
-                    let mut rgb = [0u8; 3];
-                    for i in 0..3 {
-                        let color = LinearInterpolator.interpolate(
-                            keyframe.value,
-                            root_color[i] as f32,
-                            params.boundary_set_color_rgb[i] as f32,
-                        );
-                        rgb[i] = color.clamp(0.0, 255.0).round() as u8;
-                    }
+        let mut inner_color_maps: Vec<ColorMap<LinearInterpolator>> = Vec::new();
+        let mut color_maps: Vec<ColorMapLookUpTable> = Vec::new();
 
-                    ColorMapKeyFrame {
-                        query: keyframe.query,
-                        rgb_raw: rgb,
-                    }
-                })
-                .collect();
-            inner_color_maps.push(ColorMap::new(&keyframes, LinearInterpolator));
-            color_maps.push(ColorMapLookUpTable::from_color_map(
-                inner_color_maps.last().unwrap(),
-                params.lookup_table_count,
-            ))
+        match &params.color_map_spec {
+            ColorMapSpec::GrayscaleSpec(spec) => {
+                for root_color in &spec.root_colors_rgb {
+                    let keyframes: Vec<ColorMapKeyFrame> = spec
+                        .grayscale_keyframes
+                        .iter()
+                        .map(|keyframe| {
+                            let mut rgb = [0u8; 3];
+                            for i in 0..3 {
+                                let color = LinearInterpolator.interpolate(
+                                    keyframe.value,
+                                    root_color[i] as f32,
+                                    params.boundary_set_color_rgb[i] as f32,
+                                );
+                                rgb[i] = color.clamp(0.0, 255.0).round() as u8;
+                            }
+
+                            ColorMapKeyFrame {
+                                query: keyframe.query,
+                                rgb_raw: rgb,
+                            }
+                        })
+                        .collect();
+
+                    inner_color_maps.push(ColorMap::new(&keyframes, LinearInterpolator));
+                    color_maps.push(ColorMapLookUpTable::from_color_map(
+                        inner_color_maps.last().unwrap(),
+                        params.lookup_table_count,
+                    ));
+                }
+            }
+
+            ColorMapSpec::FullColorSpec(spec) => {
+                for keyframes in spec {
+                    inner_color_maps.push(ColorMap::new(keyframes, LinearInterpolator));
+                    color_maps.push(ColorMapLookUpTable::from_color_map(
+                        inner_color_maps.last().unwrap(),
+                        params.lookup_table_count,
+                    ));
+                }
+            }
         }
+
         let histogram = Histogram::new(
             params.histogram_bin_count,
             params.max_iteration_count as f32,
